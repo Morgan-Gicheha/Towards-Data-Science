6d99ee44b5935f71f1c62e2ed926ba59c78d47cd
Morgan-Gicheha <morgangicheha4@gmail.com>
2021-04-19 11:22:19 +0300

    Delete learn.py

--- learn.py
+++
@@ -1,8 +0,0 @@
-import csv
-filename = "data.csv"
-
-with open(filename) as opened_file:
-    reade_file = csv.reader(opened_file)
-# looping throu all rows and getting the Max TemperatureF
-    for row in reade_file:
-        print(row)
